4b1702384e8cde207a6eabefe5a8844aea257468
Crowdbotics <team@crowdbotics.com>
2020-05-22 13:49:18 +0000

    Update models

--- home/migrations/0003_auto_20200522_1337.py
+++ home/migrations/0003_auto_20200522_1337.py
@@ -9,18 +9,24 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('home', '0002_load_initial_data'),
+        ("home", "0002_load_initial_data"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='customtext',
-            name='hghghg',
-            field=models.OneToOneField(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='customtext_hghghg', to=settings.AUTH_USER_MODEL),
+            model_name="customtext",
+            name="hghghg",
+            field=models.OneToOneField(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="customtext_hghghg",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
         migrations.AddField(
-            model_name='homepage',
-            name='dsfwgrf',
+            model_name="homepage",
+            name="dsfwgrf",
             field=models.BigIntegerField(blank=True, null=True),
         ),
     ]
